8993972a91bc3cfe7cec1bb202d6c82c51c4b845
lusine duryan <lusineduryan@mail.ru>
2019-08-08 16:27:07 +0400

    password checker added

---
+++ Codecademy/PasswordChecker/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            int minLength = 8;
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string uppercase = lowercase.ToUpper();
+            string digits = "0123456789";
+            string specialChars = "!~`#$%^&*?";
+            int score = 0;
+            if (password.Length >= minLength)
+            {
+                score++;
+            }
+            if (Tools.Contains(password, uppercase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, lowercase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, digits))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            }
+
+            switch (score)
+            {
+                case 4: Console.WriteLine("password is extremely strong"); break;
+                case 5: Console.WriteLine("password is extremely strong"); break;
+                case 3: Console.WriteLine("password is strong"); break;
+                case 2: Console.WriteLine("password is medium"); break;
+                case 1: Console.WriteLine("password is weak"); break;
+                default:
+                    Console.WriteLine("password doesn’t meet any of the standards");
+                    break;
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Codecademy/PasswordChecker/Tools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChecker
+{
+    class Tools
+    {
+        public static bool Contains(string target, string list)
+        {
+            for (int i = 0; i < target.Length - 1; i++)
+            {
+                for (int j = 0; j < list.Length - 1; j++)
+                {
+                    if (target[i] == list[j]) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
