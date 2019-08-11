c385e9d395568cac213bda8e2a9dfb864ceeb52e
lusine duryan <lusineduryan@mail.ru>
2019-08-11 22:47:55 +0400

    Dating Profile added

---
+++ Codecademy/TheObjectOfYourAffection/Profile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingProfile
+{
+    public class Profile
+    {
+        string name;
+        int age;
+        string city;
+        string country;
+        public string pronouns;
+        string[] hobbies;
+        public Profile (string name, int age, string city, string country, string pronouns)
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = "they/them";
+        }
+
+        public string ViewProfile()
+        {
+            string profInfo =  $"Hi, I am {name}. I am {age}. I live in {city}, {country}. I have the following hobbies: ";
+            foreach (string hobby in hobbies)
+            {
+                profInfo += hobby + ", ";
+            }
+            return profInfo;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+        
+}
+
---
+++ Codecademy/TheObjectOfYourAffection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+            string[] hobbiesOfSam = { "listening to audiobooks and podcasts", "playing rec sports like bowling and kickball",
+                "writing a speculative fiction novel" };
+            sam.SetHobbies(hobbiesOfSam);
+            Console.WriteLine(sam.ViewProfile());
+            Console.ReadKey();
+
+
+        }
+    }
+}
